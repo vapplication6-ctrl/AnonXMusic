a331667dc0b75a067f056aedd351369d758b70ce
vapplication6-ctrl <vapplication6@gmail.com>
2026-09-13 11:05:07 +0530

    Update config.py
    Signed-off-by: vapplication6-ctrl <vapplication6@gmail.com>

--- config.py
+++ config.py
@@ -5,20 +5,20 @@ load_dotenv()
 
 class Config:
     def __init__(self):
-        self.API_ID = int(getenv("API_ID", 0))
-        self.API_HASH = getenv("API_HASH")
+        self.API_ID = int(getenv("30804214", 0))
+        self.API_HASH = getenv("868c14bb613d143a45d1eaa5a17d8e1b")
 
-        self.BOT_TOKEN = getenv("BOT_TOKEN")
-        self.MONGO_URL = getenv("MONGO_URL")
+        self.BOT_TOKEN = getenv("8960458103:AAHhFY4J52hJq4z7Xk_kKiTL-g-2LEA1J-8")
+        self.MONGO_URL = getenv("mongodb+srv://arise:xenonop106@arise.a8gu8rp.mongodb.net/?appName=arise")
 
-        self.LOGGER_ID = int(getenv("LOGGER_ID", 0))
-        self.OWNER_ID = int(getenv("OWNER_ID", 0))
+        self.LOGGER_ID = int(getenv("-1004331727741", 0))
+        self.OWNER_ID = int(getenv("7983098956", 0))
 
         self.DURATION_LIMIT = int(getenv("DURATION_LIMIT", 60)) * 60
         self.QUEUE_LIMIT = int(getenv("QUEUE_LIMIT", 20))
         self.PLAYLIST_LIMIT = int(getenv("PLAYLIST_LIMIT", 20))
 
-        self.SESSION1 = getenv("SESSION", None)
+        self.SESSION1 = getenv("1BVtsOMYBuwFwVPXeMdZyjb5W0EW6lKjgxoXbhSTg2xQUhoyE18orBWxPNit8_UOEqfDhZ7u-EY5UXMZfVNz05mljiP6qxzov7T6fteAcIkIsDPXWcbM4FTXR72UiUmA72ImCoFn1vU_ts8Z6EsCryvNwaBILGYFXRlQ9E_Wll2V00IeOPneM4EeOjwmKuXEox5_SBgJ364TnbggeP-IsLb87WBKmXBkPR9lOA7QrJl6C2FEPjVToE2OaRtQZUFkZC_UTBaRLEyGimsLC4xCqk_cAmwHxfkIdVyOwFBlz7pRG27J3azr_KAMO8KRDbON9nqFODuwo_uthf8K26nhDMLlTTAg1rqc=1BVtsOMYBuwFwVPXeMdZyjb5W0EW6lKjgxoXbhSTg2xQUhoyE18orBWxPNit8_UOEqfDhZ7u-EY5UXMZfVNz05mljiP6qxzov7T6fteAcIkIsDPXWcbM4FTXR72UiUmA72ImCoFn1vU_ts8Z6EsCryvNwaBILGYFXRlQ9E_Wll2V00IeOPneM4EeOjwmKuXEox5_SBgJ364TnbggeP-IsLb87WBKmXBkPR9lOA7QrJl6C2FEPjVToE2OaRtQZUFkZC_UTBaRLEyGimsLC4xCqk_cAmwHxfkIdVyOwFBlz7pRG27J3azr_KAMO8KRDbON9nqFODuwo_uthf8K26nhDMLlTTAg1rqc=", None)
         self.SESSION2 = getenv("SESSION2", None)
         self.SESSION3 = getenv("SESSION3", None)
 
